fb267e01800f68506235ea5acdda87e6f871fc46
Carolina Herrera <ch742@cornell.edu>
2023-12-19 07:21:07 +0000

    add nokeyll file and update base configuration

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/info4340-2023sp/what-a-vue-project/' : './',
+  base: process.env.NODE_ENV === 'production' ? '/phd-visit-day/' : './',
     plugins: [
       vue(),
       Icons({
