0797f3a99480ade5de1e342b0b26bef7401dc217
caroherrera24 <115896094+caroherrera24@users.noreply.github.com>
2025-01-08 19:30:55 +0000

    trying to deploy

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/phd-visit-day/' : './',
+  base: process.env.NODE_ENV === 'github' ? '/phd-visit-day/' : './',
     plugins: [
       vue(),
       Icons({
